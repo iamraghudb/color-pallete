1f422710bd014a50f0165da793289cadd6407f5a
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-12-08 11:38:39 +0530

    Updates

---
+++ precache-manifest.2d610723f1b35b91135bda0e07dfbce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f987f7d4968dfdd1cdcb027f1248a534",
+    "url": "/color-pallete/index.html"
+  },
+  {
+    "revision": "01a53a5ca747b63c9aea",
+    "url": "/color-pallete/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-pallete/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-pallete/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a53a5ca747b63c9aea",
+    "url": "/color-pallete/static/js/main.7e3fd8ff.chunk.js"
+  },
+  {
+    "revision": "d09a1359ea33e8f3cc35",
+    "url": "/color-pallete/static/js/runtime-main.99eaccb7.js"
+  }
+]);
